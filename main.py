71e1b9586cfe6d122ec058f0819e0ebdcb5879ee
Andrew Liao <img21326@gmail.com>
2021-01-24 12:09:34 +0800

    查詢科別資料移植物件

--- main.py
+++ main.py
@@ -23,10 +23,6 @@ from _class.Hospital import KT_Hospital
 
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
-# channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-# channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 hospitals = {}
@@ -49,7 +45,7 @@ for h in hospitals:
     if (hospitals[h].channel_secret is None)  or (hospitals[h].channel_access_token is None):
         print('Specify LINE_CHANNEL_SECRET and LINE_CHANNEL_ACCESS_TOKEN as environment variables.')
         sys.exit(1)
-    hospitals[h].cache_time = cache_time
+    hospitals[h].cache_time = int(cache_time)
 if redis_host is None or redis_port is None:
     print('Specify REDIS_HOME and REDIS_PORT as environment variables.')
     sys.exit(1)
@@ -68,112 +64,6 @@ except Exception as e:
     print('pwd:' + redis_password)
     sys.exit(1)
 print('connected to redis "{}"'.format(redis_host)) 
-
-def get_doctors_url(refresh = False):
-    global redis_client
-    # if os.path.isfile("links.pickle") and (refresh == False):
-    #     with open('links.pickle', 'rb') as handle:
-    #         all_link = pickle.load(handle)
-    #     if (datetime.fromtimestamp(time.time()) - datetime.fromtimestamp(all_link['time'])).seconds > 5 * 60:
-    #         print("refresh links data")
-    #         return get_doctors_url(True)
-    #     else:
-    #         print("history links data")
-    #         return all_link
-    rlinks = redis_client.get('links')
-    if rlinks != None and refresh == False:
-        print("history links data")
-        return json.loads(rlinks)
-    else:
-        print("refresh links data")
-        r = requests.get("http://www.ktgh.com.tw/Reg_Clinic_Progress.asp?CatID=39&ModuleType=Y")
-        rt = r.text
-        rs = BeautifulSoup(rt, 'html.parser')
-        sizebox = rs.find(id='Sizebox')
-        links = sizebox.find_all(attrs={"onclick": re.compile("^javascript:location.href")})
-        all_link = {}
-        for link in links:
-            _link = (link['onclick'].split('\'')[1])
-            _title = (link.find('a')['title'])
-            all_link[_title] = _link
-        # all_link['time'] = time.time()
-        # with open('links.pickle', 'wb') as handle:
-        #     pickle.dump(all_link, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        redis_client.setex("links", cache_time,json.dumps(all_link))
-        return all_link
-
-def get_doctor_data(c, all_link,refresh = False):
-    global redis_client
-    # if os.path.isfile(c + ".pickle") and (refresh == False):
-    #     with open(c+'.pickle', 'rb') as handle:
-    #         pkl = pickle.load(handle)
-    #     if (datetime.fromtimestamp(time.time()) - datetime.fromtimestamp(pkl['time'])).seconds > 5 * 60:
-    #         print("refresh doctor data")
-    #         return get_doctor_data(c, all_link,True)
-    #     else:
-    #         print("history doctor data")
-    #         return pkl['str']
-    data = redis_client.get('doctor_' + c)
-    if data != None and refresh == False:
-        print("history doctor data")
-        data = json.loads(data)
-        return data['str']
-    else:
-        print("refresh doctor data")
-        r = requests.get("http://www.ktgh.com.tw/" + all_link[c])
-        rt = r.text
-        rs = BeautifulSoup(rt, 'html.parser')
-        _str = str(c) + '\r\n--------------------------------\r\n'
-        table = rs.find_all(attrs={'summary': '排版用表格'})[10]
-        doctors = table.find_all("a")
-        for doctor in doctors:
-            _time = doctor.parent.findNext('td')
-            if ('已' in str(_time.text)):
-                text1 = str(_time.text).split('已')[0] + '\r\n'
-                text1 = text1 + '已' + str(_time.text).split('已')[1]
-            else:
-                text1 = str(_time.text)
-
-            if ('(' in doctor.text):
-                continue
-        #     print(doctor.text)
-        #     print(_time.text)    
-            _str += doctor.text + "\r\n" + text1 + "\r\n" + "--------------------------------\r\n"
-        _str = _str + '最後更新時間:' + str(datetime.now().strftime("%Y/%m/%d %H:%M"))
-        pkl = {
-            'str': _str,
-            'time': time.time(),
-        }
-        # with open(c+'.pickle', 'wb') as handle:
-        #     pickle.dump(pkl, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        redis_client.setex("doctor_" + c, cache_time,json.dumps(pkl))
-        return _str
-
-def get_doctor_str(c):
-    all_link = get_doctors_url()
-    if str(c).isnumeric():
-        try:
-            c = int(c)
-            c = list(all_link)[c-1]
-        except Exception as e:
-            print(e)
-            c = 'error'
-    print(c)
-    if c not in all_link:
-        return "醫生還未開始看診"
-    str_ = get_doctor_data(c ,all_link)
-    return str_
-
-def get_lists_str():
-    all_links = get_doctors_url()
-    text = ""
-    i = 1
-    for a,value in all_links.items():
-        if a=='time':
-            continue
-        text += str(i) + ":" + str(a) + "\r\n"
-        i += 1
-    return text
 
 # 此為 Webhook callback endpoint
 @app.route("/callback/<hospital>", methods=['POST'])
@@ -210,7 +100,7 @@ def callback(hospital):
         if (event.message.text == '列表' or event.message.text == '0'):
             text = hospitals[hospital].crawl_list()
         else:
-            text = get_doctor_str(event.message.text)
+            text = hospitals[hospital].crawl_data(part = event.message.text)
         line_bot_api = hospitals[hospital].line_bot_api
         line_bot_api.reply_message(
             event.reply_token,
